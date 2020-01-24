2a70baa3acb067e12699646e0443625ea6036d8c
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-01-24 12:01:38 -0800

    added function for range. Almost working

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
+// Front-end logic //
+
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
     event.preventDefault();
     // Store user-input in var "value" as string //
     var value = $("#input").val();
     //store "value" in var "range" as int //
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Back-end logic //
+
+
+    // function to get range of int from 0 to int //
+    function range(start, end) {
+      var ans = [];
+      for (var i = start; i <= end; i++) {
+        ans.push(i);
+      }
+      return ans;
+    }
     var int = parseInt(value);
-    var range = range(0, int);
-    
+    var new_array = int.map(range);
+    console.log(new_array);
+
   });
 });
