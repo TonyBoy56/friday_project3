153d837540502b835e3aeb1884a0a149ddf706b2
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-01-24 16:30:51 -0800

    add nearly complete js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,47 @@
-// Front-end logic //
+////////////////////////////////// Front-end logic /////////////////////////////////////////////
 
 $(document).ready(function () {
   $("#formOne").submit(function (event) {
     event.preventDefault();
     // Store user-input in var "value" as string //
     var value = $("#input").val();
-///////////////////////////////////////////////////////////////////////////
-    // Back-end logic //
-    
 
+////////////////////////////////// Back-end logic /////////////////////////////////////////////
+    
     // function to get range of int from zero to int //
-    var range = function(zero, integer) {
+    // equal to stating "var range = function()" //
+    function range(zero, integer) {
       var array = [];
       counter = zero;
 
       while (counter <= integer) {
         array.push(counter);
-        counter++;
+        counter++; // counter = counter + 1//
       }
       return array;
     };
 
+    function replace(x) {
+      var array2 = [];
+      for (var i = 0; i <= x; i++) {
+        var newString = i.toString();
+        if (newString.includes("3")) {
+          array2[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+        } else if (newString.includes("2")) {
+          array2[i] = "Boop!";
+        } else if (newString.includes("3")) {
+          array2[i] = "Beep!";
+        }
+      }
+      return array2
+    }
+    // Establish variables containing each string //
     var integer = parseInt(value);
     var zero = parseInt(0);
-
+    // store array in "x" //
     var x = range(zero, integer);
+    var y = replace(x);
     console.log(x);
+    console.log(y);
   });
 });
