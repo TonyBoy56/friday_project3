8c002db26b62bae27968dab3ba58d5c88082e91f
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-02-02 12:33:54 -0800

    change the order of back-end to front-end logic. Front end now resides on the bottom for better organization.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,45 @@
+////////////////////////////////// Back-end logic /////////////////////////////////////////////
+
+// function to get range of int from zero to int //
+// equal to stating "var range = function()" //
+// Establish variables containing each string //
+var integer = parseInt(value);
+var zero = parseInt(0);
+// store array in "x" //
+var x = range(zero, integer);
+var y = replace(x);
+// console.log(x);
+// console.log(y);
+
+function range(zero, integer) {
+  var array = [];   
+  counter = zero;
+  while (counter <= integer) {
+    array.push(counter);
+    counter++; // counter = counter + 1//
+  }
+  return array;
+}
+
+function replace(x) {
+  // verify ifs else-ifs //
+  // run a log on the for loop to ensure its duty //
+  var array2 = [];
+  var array2 = x
+  for (var i = 0; i <= x; i++) {
+    var newString = i.toString();
+    if (newString.includes("3")) {
+      array2[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
+    } else if (newString.includes("2")) {
+      array2[i] = "Boop!";
+    } else if (newString.includes("1")) {
+      array2[i] = "Beep!";
+    }
+  }
+  console.log(array2);
+  return array2
+}
+
 ////////////////////////////////// Front-end logic /////////////////////////////////////////////
 
 $(document).ready(function () {
@@ -7,48 +49,5 @@ $(document).ready(function () {
     var value = $("#input").val();
     $("#output1").append(x);
     $("#output2").append(y);
-    
-
-////////////////////////////////// Back-end logic /////////////////////////////////////////////
-    
-    // function to get range of int from zero to int //
-    // equal to stating "var range = function()" //
-    // Establish variables containing each string //
-    var integer = parseInt(value);
-    var zero = parseInt(0);
-    // store array in "x" //
-    var x = range(zero, integer);
-    var y = replace(x);
-    // console.log(x);
-    // console.log(y);
-
-    function range(zero, integer) {
-      var array = [];   
-      counter = zero;
-      while (counter <= integer) {
-        array.push(counter);
-        counter++; // counter = counter + 1//
-      }
-      return array;
-    };
-
-    function replace(x) {
-      // verify ifs else-ifs //
-      // run a log on the for loop to ensure its duty //
-      var array2 = [];
-      var array2 = x
-      for (var i = 0; i <= x; i++) {
-        var newString = i.toString();
-        if (newString.includes("3")) {
-          array2[i] = "I'm sorry, Dave. I'm afraid I can't do that.";
-        } else if (newString.includes("2")) {
-          array2[i] = "Boop!";
-        } else if (newString.includes("1")) {
-          array2[i] = "Beep!";
-        }
-      }
-      console.log(array2);
-      return array2
-    }
   });
 });
